27cc1a359bf720503ac9eb3895fd475f77d3bddc
Ahsan196 <ahsan.mansoor1996@gmail.com>
2020-10-12 23:09:02 +0500

    Changes to allow deployment on heroku

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true,useCreateIndex:true,useUnifiedTopology: true ,useFindAndModify:false}
-);
+const uri =
+  "mongodb+srv://ahsan:ahsan1396@resumeportfolio.nom6q.mongodb.net/projects?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-}) 
+});
 
-const projectsRouter = require('./routes/projects');
+const projectsRouter = require("./routes/projects");
 
-app.use('/projects', projectsRouter);
+app.use("/projects", projectsRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
